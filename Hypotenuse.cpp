d7040442c2d71f13622fa27f638beb891f31a361
Varun Bhalerao <your@email.com>
2025-10-30 12:33:43 +0100

    First Commit

---
+++ Hypotenuse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int Month;
+    cout << "Enter a Month : ";
+    cin >> Month;
+
+    switch (Month) {
+        case 1: cout << "January"; break;
+        case 2: cout << "February"; break;
+        case 3: cout << "March"; break;
+        case 4: cout << "April"; break;
+        case 5: cout << "May"; break;
+        case 6: cout << "June"; break;
+        case 7: cout << "July"; break;
+        case 8: cout << "August"; break;
+        case 9: cout << "September"; break;
+        case 10: cout << "October"; break;
+        case 11: cout << "November"; break;
+        case 12: cout << "December"; break;
+        default: cout << "Please enter numbers between 1 to 12 only"; break;
+    }
+
+    return 0;
+
+
+
+
+
+}
