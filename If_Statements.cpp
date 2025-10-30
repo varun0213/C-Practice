876847de7ebed3a809ea39707fd7ee91b146a389
Varun Bhalerao <your@email.com>
2025-10-30 12:46:24 +0100

    If Statements

---
+++ If_Statements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    if (name == "Varun") {
+        cout << " Your apartment is 96!";
+    }
+    else if (name == "Ayush") {
+        cout << " Your Apartment is 143!";
+    }
+    else
+        cout<<"Your don't live in this building";
+    return 0;
+
+
+
+
+
+}
